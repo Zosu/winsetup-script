cd23a2923fe3b417064125a4a0738c512ffb846b
Zosu <56417732+Zosu@users.noreply.github.com>
2024-01-05 17:26:09 -0500

    Update setupscript.bat

--- setupscript.bat
+++ setupscript.bat
@@ -2,7 +2,6 @@
 REM Just the start for the script chain so no execution policy needs to be changed and for easier use
 powershell.exe -executionpolicy bypass -file "Script_Files\setup_ps_script.ps1"
 
-sfc /scannow
 echo Please restart to apply all changes
 echo Don't forget to check startup programs!
 pause
